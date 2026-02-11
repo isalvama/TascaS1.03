612bcf2df2ef62c853125dcede00580c982576e3
isalvama <inesalvans@gmail.com>
2026-02-11 09:59:21 +0100

    feat: create CountriesAndCapitalsData.java class to keep the name and points of the player when the method program of GameProgram is running

---
+++ src/main/java/first_level/ex3/Game.java
@@ -0,0 +1,39 @@
+package first_level.ex3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Game {
+    private String userName;
+    private int points;
+
+
+    public Game(String userName) {
+        this.userName = userName;
+        this.points = 0;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+    public int setPoints(int points) {
+        return this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "userName='" + userName + '\'' +
+                ", points=" + points +
+                '}';
+    }
+}
