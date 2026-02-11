faeaf4145fa77528bbcc900c702d0f0c84a28fd3
isalvama <inesalvans@gmail.com>
2026-02-11 17:35:27 +0100

    feat: add comments for better understanding of code in SecondExMain.java

--- src/main/java/first_level/ex2/SecondExMain.java
+++ src/main/java/first_level/ex2/SecondExMain.java
@@ -4,23 +4,22 @@ import java.util.*;
 
 public class SecondExMain {
     public static void main(String[] args){
+        // Create a List<Integer> and add elements
         List<Integer> listA= List.of(1, 2, 3, 4);
 
         System.out.println(listA);
 
-        // Create a list iterator over the elements in the listA (in proper sequence), starting at the last position in the list. An initial call to previous would return the element with the specified index minus one.
-        ListIterator<Integer> listIterator = listA.listIterator(listA.size());
+        // Create a new ArrayList to insert the elements of listA in reverse order
         List<Integer> listB = new ArrayList<>();
 
+        // Create a list iterator over the elements in the listA (in proper sequence), placing the cursor at the last position in the list.
+        ListIterator<Integer> listIterator = listA.listIterator(listA.size());
+
+
+        // Add the elements of listA in listB in reverse order: while there is an element behind, the previous element is being added in listB
         while(listIterator.hasPrevious()){
             listB.add(listIterator.previous());
         }
-
         System.out.println(listB);
-
-
-
-
-
     }
 }
