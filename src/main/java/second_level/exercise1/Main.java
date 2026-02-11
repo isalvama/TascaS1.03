3e7a06e38767d15c89c26a79eda1d58ca2abcfa0
isalvama <inesalvans@gmail.com>
2026-02-11 17:20:10 +0100

    fix: correct data in attributes and create new objects with same duplicated data for better testing in Main.java file of exercise 1 second_level package feat: call sort method to use compareTo() implementation in Restaurant class

--- src/main/java/second_level/exercise1/Main.java
+++ src/main/java/second_level/exercise1/Main.java
@@ -1,19 +1,31 @@
 package second_level.exercise1;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args){
+
+        // Exercise 1 instructions
         Restaurant rest1 = new Restaurant("rest1", 3);
-        Restaurant rest2 = new Restaurant("rest1", 5);
+        Restaurant rest2 = new Restaurant("rest1", 3);
         Restaurant rest3 = new Restaurant("rest3", 4);
+        Restaurant rest4 = new Restaurant("rest4", 5);
+        Restaurant rest5 = new Restaurant("rest4", 5);
 
         HashSet<Restaurant> restaurantsSet = new HashSet<>();
         restaurantsSet.add(rest1);
-        restaurantsSet.add(rest1);
+        restaurantsSet.add(rest3);
         restaurantsSet.add(rest2);
-        restaurantsSet.add(rest3);
-        restaurantsSet.add(rest3);
+        restaurantsSet.add(rest4);
+        restaurantsSet.add(rest5);
         System.out.println(restaurantsSet);
+
+        // Exercise 2 instructions
+        ArrayList<Restaurant> restaurantArrayList = new ArrayList<>(restaurantsSet);
+
+        Collections.sort(restaurantArrayList);
+        System.out.println(restaurantArrayList);
 
 
 
