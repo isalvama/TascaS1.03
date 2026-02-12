2cc8440977f3192ac04056ead16e227dde0b195f
isalvama <inesalvans@gmail.com>
2026-02-12 09:38:33 +0100

    feat: create more Restaurant objects to test Comparator method when calling Collections.sort, change params when calling sort method

--- src/main/java/second_level/exercise1/Main.java
+++ src/main/java/second_level/exercise1/Main.java
@@ -7,11 +7,16 @@ public class Main {
     public static void main(String[] args){
 
         // Exercise 1 instructions
-        Restaurant rest1 = new Restaurant("rest1", 3);
-        Restaurant rest2 = new Restaurant("rest1", 3);
-        Restaurant rest3 = new Restaurant("rest3", 4);
-        Restaurant rest4 = new Restaurant("rest4", 5);
-        Restaurant rest5 = new Restaurant("rest4", 5);
+        Restaurant rest1 = new Restaurant("restA", 3);
+        Restaurant rest2 = new Restaurant("restA", 3);
+        Restaurant rest3 = new Restaurant("restC", 4);
+        Restaurant rest4 = new Restaurant("restD", 5);
+        Restaurant rest5 = new Restaurant("restD", 5);
+        Restaurant rest6 = new Restaurant("restA", 2);
+        Restaurant rest7 = new Restaurant("restD", 4);
+        Restaurant rest8 = new Restaurant("restD", 3);
+
+
 
         HashSet<Restaurant> restaurantsSet = new HashSet<>();
         restaurantsSet.add(rest1);
@@ -19,12 +24,16 @@ public class Main {
         restaurantsSet.add(rest2);
         restaurantsSet.add(rest4);
         restaurantsSet.add(rest5);
+        restaurantsSet.add(rest6);
+        restaurantsSet.add(rest7);
+        restaurantsSet.add(rest8);
+
         System.out.println(restaurantsSet);
 
         // Exercise 2 instructions
         ArrayList<Restaurant> restaurantArrayList = new ArrayList<>(restaurantsSet);
 
-        Collections.sort(restaurantArrayList);
+        Collections.sort(restaurantArrayList, new NameCompare());
         System.out.println(restaurantArrayList);
 
 
