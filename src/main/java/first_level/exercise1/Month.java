d2b5448a7a972de8224ede7c5e467a6f9c3f4491
isalvama <inesalvans@gmail.com>
2026-02-16 22:35:50 +0100

    fix: Generate equals and hashCode methods for Month objects with the same name not te be added in the same Set

--- src/main/java/first_level/exercise1/Month.java
+++ src/main/java/first_level/exercise1/Month.java
@@ -1,7 +1,21 @@
 package first_level.exercise1;
 
+import java.util.Objects;
+
 public class Month {
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return Objects.equals(name, month.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 
     public Month(String name){
         this.name = name;
