a481000370f87dc174fbdb764624d8043a9cce12
isalvama <inesalvans@gmail.com>
2026-02-17 13:50:21 +0100

    doc: update ThirdExMain class according to last changes

--- src/main/java/first_level/exercise3/ThirdExMain.java
+++ src/main/java/first_level/exercise3/ThirdExMain.java
@@ -4,7 +4,10 @@ package first_level.exercise3;
 public class ThirdExMain {
     public static void main(String[] args) {
 
-        Game game = GameProgram.program();
+        GameMachine game = new GameMachine();
+        game.program();
+        game.program();
+        System.out.println(game.getPlayedGames());
 
     }
 
