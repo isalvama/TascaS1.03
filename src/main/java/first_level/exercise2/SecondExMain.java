7ec8a472715f3e858959cefdc93368b48ccd4ff0
isalvama <inesalvans@gmail.com>
2026-02-18 15:41:22 +0100

    feat: refactor SecondExMain.java code to unify all the core logic in a new method (sortInReverseOrder)

--- src/main/java/first_level/exercise2/SecondExMain.java
+++ src/main/java/first_level/exercise2/SecondExMain.java
@@ -4,18 +4,19 @@ import java.util.*;
 
 public class SecondExMain {
     public static void main(String[] args){
-        List<Integer> listA= List.of(1, 2, 3, 4);
+        List<Integer> listA = List.of(1, 2, 3, 4);
+        System.out.println("list A: " + listA);
 
-        System.out.println(listA);
+        List<Integer> listB = sortInReverseOrder(listA);
+        System.out.println("list B: " + listB);
+    }
 
+    public static List<Integer> sortInReverseOrder(List<Integer> listA){
         List<Integer> listB = new ArrayList<>();
-
         ListIterator<Integer> listIterator = listA.listIterator(listA.size());
-
         while(listIterator.hasPrevious()){
             listB.add(listIterator.previous());
         }
-
-        System.out.println(listB);
+        return listB;
     }
 }
