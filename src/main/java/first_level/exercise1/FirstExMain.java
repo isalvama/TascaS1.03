33cf6642d30058b190caf77142eb3a8306274bd9
isalvama <inesalvans@gmail.com>
2026-02-18 15:29:00 +0100

    format: remove unnecessary spaces

--- src/main/java/first_level/exercise1/FirstExMain.java
+++ src/main/java/first_level/exercise1/FirstExMain.java
@@ -10,29 +10,23 @@ public class FirstExMain {
 
         Month august = new Month("August");
 
-        List<Month> monthArrayList = new ArrayList<Month>(List.of(new Month("January"), new Month("February"), new Month("March"), new Month("April"), new Month("May"), new Month("June"), new Month("July"), new Month("September"),
-       new Month("October"),
+        List<Month> monthArrayList = new ArrayList<>(List.of(new Month("January"), new Month("February"), new Month("March"), new Month("April"), new Month("May"), new Month("June"), new Month("July"), new Month("September"),
+        new Month("October"),
         new Month("November"),
         new Month("December")));
-
-
         System.out.println("monthArrayList without 'August': " + monthArrayList);
 
         monthArrayList.add(7, august);
         System.out.println("monthArrayList after adding 'August' in position 7: " + monthArrayList);
 
         HashSet<Month> monthHashSet = convertMonthArrayListToHashSet(monthArrayList);
-
         System.out.println("monthHashSet: " + monthHashSet);
 
         monthHashSet.add(august);
-
         System.out.println("monthHashSet after adding duplicate august: " + monthHashSet);
 
         Month inventedMonth = new Month("Invented Month");
-
         monthHashSet.add(inventedMonth);
-
         System.out.println("monthHashSet after adding invented month: " + monthHashSet);
 
         System.out.println("------- loop with a for--------");
