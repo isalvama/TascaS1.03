051da1183fe94203dedf15a7e3187129120cddaf
isalvama <inesalvans@gmail.com>
2026-02-18 16:19:51 +0100

    refactor: change order of methods in GameMachine class for better readability fix: format the user answer and the game answer to lower case for better comparability

--- src/main/java/first_level/exercise3/GameMachine.java
+++ src/main/java/first_level/exercise3/GameMachine.java
@@ -10,11 +10,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GameMachine {
-    private final ArrayList<String> countriesList;
-    private final HashMap<String, String> countriesCapitalsMap;
+    private ArrayList<String> countriesList;
+    private HashMap<String, String> countriesCapitalsMap;
     private static final Scanner SC = new Scanner(System.in);
     private ArrayList<Game> playedGames = new ArrayList<>();
-    private static final int N_QUESTIONS = 1;
+    private static final int N_QUESTIONS = 10;
 
     public List<Game> getPlayedGames() {
         return List.copyOf(playedGames);
@@ -23,29 +23,7 @@ public class GameMachine {
     public GameMachine(){
         this.countriesList = new ArrayList<String>();
         this.countriesCapitalsMap = new HashMap<String, String>();
-    }
-
-    public void loadCountries(){
-        String path = System.getProperty("user.dir") + File.separator + "countries.txt";
-        File file = new File(path);
-
-        if(!file.exists()){
-            System.err.println("File not found");
-        }
-            try(BufferedReader br = new BufferedReader(new FileReader(file))){
-                String line;
-                while ((line = br.readLine()) != null) {
-                    String[] parts = line.split(" ");
-                    String country = parts[0].trim();
-                    String capital = parts[1].trim();
-                    if (!country.isEmpty() && !capital.isEmpty()){
-                        countriesCapitalsMap.put(country, capital);
-                        countriesList.add(country);
-                    }
-                }
-            } catch (IOException e) {
-                System.err.println("Error loading countries " + e.getMessage());
-            }
+        this.playedGames = new ArrayList<Game>();
     }
 
         public void program(){
@@ -59,17 +37,46 @@ public class GameMachine {
             int index = (int)(Math.random() * countriesList.size());
             String questionCountry = countriesList.get(index);
             System.out.println("Which is the capital of " + questionCountry + " ?");
-            String answer = SC.nextLine();
-            String formattedAnswer = answer.substring(0, 1).toUpperCase() + answer.substring(1);
-            if (formattedAnswer.equals(countriesCapitalsMap.get(questionCountry))){
+            String answer = SC.nextLine().toLowerCase();
+            String formattedAnswer = answer.toLowerCase();
+            if (formattedAnswer.equals(countriesCapitalsMap.get(questionCountry).toLowerCase())){
                 points++;
                 System.out.println("Congrats! You got the right answer! You now have " + points + " points.");
             } else {
             System.out.println("Your answer was not right. The right answer was: " + countriesCapitalsMap.get(questionCountry));
             }
         }
-            System.out.println("Game over! You got " + " points.");
+            System.out.println("Game over! You got " + points + " points.");
             game.setPoints(points);
             playedGames.add(game);
+            seePlayedGames();
+    }
+
+    public void loadCountries(){
+        String path = System.getProperty("user.dir") + File.separator + "countries.txt";
+        File file = new File(path);
+
+        if(!file.exists()){
+            System.err.println("File not found");
+        }
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String country = parts[0].trim().replace('_',' ');
+                String capital = parts[1].trim().replace('_',' ');;
+                if (!country.isEmpty() && !capital.isEmpty()){
+                    countriesCapitalsMap.put(country, capital);
+                    countriesList.add(country);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading countries " + e.getMessage());
+        }
+    }
+
+
+    public String seePlayedGames() {
+        return "All the scores:\n" + List.copyOf(playedGames);
     }
 }
